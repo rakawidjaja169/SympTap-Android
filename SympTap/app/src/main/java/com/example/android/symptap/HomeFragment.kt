291f5bebc896cf893280169b2750a9eacaaf46a4
rakawidjaja169 <54425993+rakawidjaja169@users.noreply.github.com>
2020-11-04 22:28:26 +0700

    Clicked CardView

--- SympTap/app/src/main/java/com/example/android/symptap/HomeFragment.kt
+++ SympTap/app/src/main/java/com/example/android/symptap/HomeFragment.kt
@@ -1,12 +1,12 @@
 package com.example.android.symptap
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_home.*
+
 
 /**
  * A simple [Fragment] subclass.
@@ -14,9 +14,16 @@ import kotlinx.android.synthetic.main.fragment_home.*
 class HomeFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        diagnose_card.setOnClickListener{
+        }
     }
 }
