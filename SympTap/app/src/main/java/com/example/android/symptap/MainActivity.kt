25ae3fe9e26b72172dd119912ba920da08f63555
rakawidjaja169 <54425993+rakawidjaja169@users.noreply.github.com>
2020-11-04 21:45:38 +0700

    UI

---
+++ SympTap/app/src/main/java/com/example/android/symptap/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android.symptap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.cardview.widget.CardView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
