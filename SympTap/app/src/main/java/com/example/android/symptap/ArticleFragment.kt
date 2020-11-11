147f2f12e690551592413f15c496eaa35a069e31
rakawidjaja169 <54425993+rakawidjaja169@users.noreply.github.com>
2020-11-11 12:55:37 +0700

    Adding Material View for Article
    Add COVID-19 Fragment

--- SympTap/app/src/main/java/com/example/android/symptap/ArticleFragment.kt
+++ SympTap/app/src/main/java/com/example/android/symptap/ArticleFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_article.*
+import kotlinx.android.synthetic.main.fragment_home.*
 
 /**
  * A simple [Fragment] subclass.
@@ -16,5 +19,13 @@ class ArticleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_article, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        coronavirusCard.setOnClickListener{
+            view.findNavController().navigate(R.id.action_articleFragment_to_coronavirusFragment)
+        }
     }
 }
